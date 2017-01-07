3465eade8e2b7b529dd12f542305b615c9e18b8b
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-08 02:56:57 +0400

    Fix of NPE while inject of EntityManager

--- src/com/slamur/app/neuro/dao/AlgorithmDaoJPA.java
+++ src/com/slamur/app/neuro/dao/AlgorithmDaoJPA.java
@@ -1,10 +1,12 @@
 package com.slamur.app.neuro.dao;
 
 import com.slamur.app.neuro.model.AlgorithmEntity;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Component
 public class AlgorithmDaoJPA
         extends NeuroDictionaryDaoImpl<AlgorithmEntity>
         implements AlgorithmDao {

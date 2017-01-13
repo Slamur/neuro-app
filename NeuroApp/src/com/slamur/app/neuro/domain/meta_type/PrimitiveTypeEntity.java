46ffc738ac5f796135b26030211221f075d3adbc
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-13 18:55:16 +0400

    Added PrimitiveTypeEntity for table 'primitive_type'

---
+++ NeuroApp/src/com/slamur/app/neuro/domain/meta_type/PrimitiveTypeEntity.java
@@ -0,0 +1,34 @@
+package com.slamur.app.neuro.domain.meta_type;
+
+import com.slamur.lib.domain.impl.DomainEntityImpl;
+import com.slamur.lib.domain.impl.DomainNamedEntityImpl;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "primitive_type", schema = "public", catalog = "neuroapp")
+public class PrimitiveTypeEntity extends DomainNamedEntityImpl {
+
+    public Integer id;
+    public String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

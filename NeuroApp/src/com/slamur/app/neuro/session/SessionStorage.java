7394e7bceb421de95d378caf60c3bba9e536084b
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-13 18:15:47 +0400

    Added SessionStorage class with session scope

---
+++ NeuroApp/src/com/slamur/app/neuro/session/SessionStorage.java
@@ -0,0 +1,20 @@
+package com.slamur.app.neuro.session;
+
+import com.slamur.app.neuro.domain.meta_type.DictionaryTypeEntity;
+import com.slamur.lib.domain.DomainEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface SessionStorage {
+
+    boolean isEntityCreating();
+    void setEntityCreating(boolean entityCreating);
+
+    DomainEntity getEditingEntity();
+    void setEditingEntity(DomainEntity entity);
+
+    List<DictionaryTypeEntity> getDictionaryTypes();
+    void setDictionaryTypes(List<DictionaryTypeEntity> dictionaryTypes);
+}
---
+++ NeuroApp/src/com/slamur/app/neuro/session/impl/SessionStorageImpl.java
@@ -0,0 +1,47 @@
+package com.slamur.app.neuro.session.impl;
+
+import com.slamur.app.neuro.domain.meta_type.DictionaryTypeEntity;
+import com.slamur.app.neuro.session.SessionStorage;
+import com.slamur.lib.domain.DomainEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SessionStorageImpl implements SessionStorage {
+
+    private boolean isEntityCreating;
+    private DomainEntity editingEntity;
+
+    private List<DictionaryTypeEntity> dictionaryTypes;
+
+    @Override
+    public boolean isEntityCreating() {
+        return isEntityCreating;
+    }
+
+    @Override
+    public void setEntityCreating(boolean entityCreating) {
+        isEntityCreating = entityCreating;
+    }
+
+    @Override
+    public DomainEntity getEditingEntity() {
+        return editingEntity;
+    }
+
+    @Override
+    public void setEditingEntity(DomainEntity editingEntity) {
+        this.editingEntity = editingEntity;
+    }
+
+    @Override
+    public List<DictionaryTypeEntity> getDictionaryTypes() {
+        return dictionaryTypes;
+    }
+
+    @Override
+    public void setDictionaryTypes(List<DictionaryTypeEntity> dictionaryTypes) {
+        this.dictionaryTypes = dictionaryTypes;
+    }
+}

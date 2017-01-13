ea9832e683a7ac70ad81a138517ece409103e511
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-14 03:24:04 +0400

    Added QueryModel

---
+++ NeuroApp/src/com/slamur/app/neuro/domain/query/QueryModel.java
@@ -0,0 +1,12 @@
+package com.slamur.app.neuro.domain.query;
+
+public class QueryModel {
+
+    public QueryModel() {
+
+    }
+
+    public QueryModel(QueryEntity query) {
+        this();
+    }
+}

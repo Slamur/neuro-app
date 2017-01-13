24d38ff733e75d09924979df0fcc2f2e984792e8
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-13 17:50:13 +0400

    Removed tables 'algorithm' and 'network'

--- NeuroApp/src/com/slamur/app/neuro/dao/AlgorithmDao.java
+++
@@ -1,8 +0,0 @@
-package com.slamur.app.neuro.dao;
-
-import com.slamur.app.neuro.domain.AlgorithmEntity;
-import com.slamur.lib.dao.DomainDictionaryDao;
-
-public interface AlgorithmDao extends DomainDictionaryDao<AlgorithmEntity> {
-
-}
--- NeuroApp/src/com/slamur/app/neuro/dao/NetworkDao.java
+++
@@ -1,8 +0,0 @@
-package com.slamur.app.neuro.dao;
-
-import com.slamur.app.neuro.domain.NetworkEntity;
-import com.slamur.lib.dao.DomainDictionaryDao;
-
-public interface NetworkDao extends DomainDictionaryDao<NetworkEntity> {
-
-}

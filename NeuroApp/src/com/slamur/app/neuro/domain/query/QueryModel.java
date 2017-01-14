f19fea5715b4b363ba94bd51ca70db3e612ac5c2
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-14 06:37:03 +0400

    Update query model

--- NeuroApp/src/com/slamur/app/neuro/domain/query/QueryModel.java
+++ NeuroApp/src/com/slamur/app/neuro/domain/query/QueryModel.java
@@ -1,10 +1,6 @@
 package com.slamur.app.neuro.domain.query;
 
-import java.util.List;
-
 public class QueryModel {
-
-    private List<QueryParameterEntity> parameters;
 
     public QueryModel() {
 
@@ -12,14 +8,5 @@ public class QueryModel {
 
     public QueryModel(QueryEntity query) {
         this();
-        this.parameters = query.getParameters();
-    }
-
-    public List<QueryParameterEntity> getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(List<QueryParameterEntity> parameters) {
-        this.parameters = parameters;
     }
 }

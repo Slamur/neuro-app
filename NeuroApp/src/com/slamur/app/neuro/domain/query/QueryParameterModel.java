b20707d2b59953cae83069643a2dd1127ccbd7b5
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-01-14 05:02:45 +0400

    Added QueryParameterModel

---
+++ NeuroApp/src/com/slamur/app/neuro/domain/query/QueryParameterModel.java
@@ -0,0 +1,52 @@
+package com.slamur.app.neuro.domain.query;
+
+public class QueryParameterModel {
+
+    private int parameterId;
+    private String value;
+
+    public QueryParameterModel() {
+
+    }
+
+    public QueryParameterModel(QueryParameterEntity queryParameter) {
+        this();
+
+        this.parameterId = queryParameter.getParameter().getId();
+        this.value = queryParameter.getValue();
+    }
+
+    public int getParameterId() {
+        return parameterId;
+    }
+
+    public void setParameterId(int parameterId) {
+        this.parameterId = parameterId;
+    }
+
+    public String getStringValue() {
+        return value == null ? "" : value;
+    }
+
+    public void setStringValue(String value) {
+        this.value = value;
+    }
+
+    public int getIntValue() {
+        return (value == null || value.isEmpty())
+                ? 0
+                : Integer.parseInt(value);
+    }
+
+    public void setIntValue(int value) {
+        this.value = Integer.toString(value);
+    }
+
+    public boolean getBooleanValue() {
+        return !(value == null || value.isEmpty()) && Boolean.parseBoolean(value);
+    }
+
+    public void setBooleanValue(boolean value) {
+        this.value = Boolean.toString(value);
+    }
+}
